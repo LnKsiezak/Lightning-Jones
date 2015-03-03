d7cfd52ca5c27c2a9bc332facd76826ef0ac78fc
feedthejim <feed.the.jim@gmail.com>
2015-03-03 18:25:51 +0100

    Light tweaked+ character playable
    Physics needs to be tweaked

--- Assets/Character/player_manager.cs
+++ Assets/Character/player_manager.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class player_manager : MonoBehaviour {
     public float speed;
     public bool onground;
+
+    public float left_lane = -0.5F, middle_lane = 0.5F, right_lane = 1.5F,current_pos= 1.5F;
+    public float jump_force = 3;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -16,15 +20,42 @@ public class player_manager : MonoBehaviour {
             transform.Translate(new Vector3(0, 0, speed * Input.GetAxis("Horizontal")) * Time.deltaTime);
 
         }
-        if (Input.GetButtonDown("Vertical") && ( -2 < rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical")))  && ( 2 > rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical"))))
-        {
+        //if (Input.GetButtonDown("Vertical") && ( -2 < rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical")))  && ( 2 > rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical"))))
+        //{
            // transform.Translate(new Vector3(0, 0,  Input.GetAxis("Vertical")));
             //transform.Translate(new Vector3(0, 0, speed * Input.GetAxis("Vertical")) * Time.deltaTime);
-            rigidbody.MovePosition(rigidbody.position + new Vector3(0,0,2*Mathf.Sign(Input.GetAxis("Vertical"))));
+         //   rigidbody.MovePosition(rigidbody.position + new Vector3(0,0,2*Mathf.Sign(Input.GetAxis("Vertical"))));
+       // }
+        if(Input.GetButtonDown("Vertical"))
+        {
+            if(Mathf.Sign(Input.GetAxis("Vertical"))==-1)
+            {
+                if(current_pos==middle_lane)
+                {
+                    current_pos = right_lane;
+                }
+                else if(current_pos==left_lane)
+                {
+                    current_pos = middle_lane;
+                }
+            }
+            else if (Mathf.Sign(Input.GetAxis("Vertical"))==1)
+            {
+                if(current_pos == right_lane)
+                {
+                    current_pos = middle_lane;
+                }
+                else if(current_pos == middle_lane)
+                {
+                    current_pos = left_lane;
+                }
+            }
+            transform.position = new Vector3(current_pos, transform.position.y, transform.position.z);
         }
         if(Input.GetButton("Jump")&& onground)
         {
-            rigidbody.AddForce(0,3,0,ForceMode.Impulse);
+            rigidbody.AddForce(Vector3.up * jump_force,ForceMode.Impulse);
+          //  transform.Translate(Vector3.up * Time.deltaTime);
         }
 	}
     void OnCollisionEnter ()
