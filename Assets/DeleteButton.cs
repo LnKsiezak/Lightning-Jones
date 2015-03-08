6f8258bf8a7c67d0e5d9df22a87252904128f5e4
WillZillah <wlim.612@gmail.com>
2015-03-08 21:17:44 +0100

    boutton menu

---
+++ Assets/DeleteButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteButton : MonoBehaviour
+{
+
+    bool enable = true;
+    Rect button1 = new Rect(350, 200, 150, 70);
+
+    void OnGUI()
+    {
+               
+        if (enable)
+        {
+            Time.timeScale = 0;
+            if (GUI.Button(button1, "Play Game"))
+            {
+                Time.timeScale = 1;
+                enable = false;
+                
+            }
+        }
+    }
+}
+  
+
+ 
