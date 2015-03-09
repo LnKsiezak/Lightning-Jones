2b9423e53611e1854c8871618de272de35231b52
WillZillah <wlim.612@gmail.com>
2015-03-09 13:13:16 +0100

    BAILS A WILLIAM
    j'ai tout commit oui tg jim

---
+++ Assets/pausemenu.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+var pauseCanvas : Canvas;
+
+function Start () {
+    Screen.lockCursor = true;
+    Screen.showCursor = false;
+}
+
+function Update () {
+    if(Input.GetKeyDown(KeyCode.P))
+    {      
+        
+        pauseCanvas.enabled = true;
+        Time.timeScale = 0;
+        Screen.lockCursor = false;
+        Screen.showCursor = true;
+    }
+}
+function ResumeGame (){    
+    
+   pauseCanvas.enabled = false;
+    Time.timeScale = 1;
+    Screen.lockCursor = true;
+    Screen.showCursor = false;
+} 
+
+function ExitGame () {
+Application.Quit ();
+}
+
+function ExitMenu(){
+Application.LoadLevel("main-menu");
+pauseCanvas.enabled = false;
+Time.timeScale = 1;
+}
