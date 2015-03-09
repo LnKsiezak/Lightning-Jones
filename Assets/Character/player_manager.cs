972b033b79dc49834fac78e6977764454b90a8cb
feedthejim <feed.the.jim@gmail.com>
2015-03-09 13:24:35 +0100

    better lane mov

--- Assets/Character/player_manager.cs
+++ Assets/Character/player_manager.cs
@@ -5,20 +5,22 @@ public class player_manager : MonoBehaviour {
     public float speed;
     public bool onground;
 
-    public float left_lane = -0.5F, middle_lane = 0.5F, right_lane = 1.5F,current_pos= 1.5F;
+    public float left_lane = -0.5F, middle_lane = 0.5F, right_lane = 1.5F;
     public float jump_force = 3;
 
+    private Vector3  previous_pos = new Vector3();
+    private float current_pos;
 	// Use this for initialization
 	void Start () {
-	
+        previous_pos = transform.position;
+        current_pos = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(Input.GetButton("Horizontal"))
         {
-            transform.Translate(new Vector3(0, 0, speed * Input.GetAxis("Horizontal")) * Time.deltaTime);
-
+            transform.Translate(new Vector3(0, 0, speed * Mathf.Sign(Input.GetAxis("Horizontal"))) * Time.deltaTime);
         }
         //if (Input.GetButtonDown("Vertical") && ( -2 < rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical")))  && ( 2 > rigidbody.position.z + Mathf.Sign(Input.GetAxis("Vertical"))))
         //{
@@ -50,13 +52,18 @@ public class player_manager : MonoBehaviour {
                     current_pos = left_lane;
                 }
             }
-            transform.position = new Vector3(current_pos, transform.position.y, transform.position.z);
+           // transform.position = new Vector3(current_pos, transform.position.y, transform.position.z);
         }
         if(Input.GetButton("Jump")&& onground)
         {
             rigidbody.AddForce(Vector3.up * jump_force,ForceMode.Impulse);
           //  transform.Translate(Vector3.up * Time.deltaTime);
         }
+
+
+        transform.position = new Vector3(Mathf.Lerp(previous_pos.x, current_pos, speed*Time.deltaTime),transform.position.y,transform.position.z);
+
+        previous_pos = transform.position;
 	}
     void OnCollisionEnter ()
     {
