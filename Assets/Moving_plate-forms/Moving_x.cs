660486be41a94482586a949d056e2c496525177f
shiinsekai <quangnghi.huynh@epita.fr>
2015-04-04 15:34:13 +0200

    Adding moving_x and moving_y scripts

---
+++ Assets/Moving_plate-forms/Moving_x.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving_x : MonoBehaviour 
+{	
+	public float speed;
+	public float left_limit;
+	public float right_limit;
+	private bool go_right = true;
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((transform.position.x <= left_limit) || (transform.position.x >= right_limit)) 
+		{
+			if(transform.position.x <= left_limit)
+			{
+				go_right = true;
+			}
+			else
+			{
+				go_right = false;
+			}
+		}
+		if (go_right) 
+		{
+			transform.Translate (new Vector3(speed,0.0F,0.0F));
+			go_right = true;
+		} 
+		else 
+		{
+			transform.Translate(new Vector3(-speed,0.0F,0.0F));
+			go_right = false;
+		}
+
+	}
+}
---
+++ Assets/Moving_plate-forms/Moving_y.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving_y : MonoBehaviour {
+
+	public float speed;
+	public float up_limit;
+	public float down_limit;
+	private bool go_up = true;
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((transform.position.y <= down_limit) || (transform.position.y >= up_limit)) 
+		{
+			if (transform.position.y <= down_limit) 
+			{
+				go_up = true;
+			} 
+			else 
+			{
+				go_up = false;
+			}
+		}
+		if (go_up) 
+		{
+			transform.Translate (new Vector3 (0.0F, speed, 0.0F));
+			go_up = true;
+		} 
+		else 
+		{
+			transform.Translate (new Vector3 (0.0F, -speed, 0.0F));
+			go_up = false;
+		}
+	}
+}
