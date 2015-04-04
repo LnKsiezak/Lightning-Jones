1716b72bfefd4d5faf9542216955fc9f9a389c32
shiinsekai <quangnghi.huynh@epita.fr>
2015-04-04 19:17:21 +0200

    Moving blocks changed

---
+++ Assets/Moving_plate-forms/Moving_z.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving_z : MonoBehaviour {
+
+	public float speed;
+	public float front_limit;
+	public float rear_limit;
+	private bool go_rear = true;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((transform.position.z <= front_limit) || (transform.position.z >= rear_limit)) 
+		{
+			if (transform.position.z <= front_limit) 
+			{
+				go_rear = true;
+			} 
+			else 
+			{
+				go_rear = false;
+			}
+		}
+		if (go_rear) 
+		{
+			transform.Translate (new Vector3 (0.0F, 0.0F, speed));
+			go_rear = true;
+		} 
+		else 
+		{
+			transform.Translate (new Vector3 (0.0F, 0.0F, -speed));
+			go_rear = false;
+		}
+	
+	}
+}
